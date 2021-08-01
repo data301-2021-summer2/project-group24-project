0b08efb6d5d4867b6bafd523ca15e4dfb4f1f8cc
Fei Song <feifeisophie_song@hotmail.com>
2021-08-01 12:26:26 -0700

    .py file updated

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
 def load_and_process(url_or_path_to_csv_file):
-    df1=pd.read_csv(url_or_path_to_csv_file).rename(columns={'age':'Age','sex':'Sex','bmi':'BMI','children':'Children','smoker':'Smoker','region':'Region','charges':'Insurance Premium'}).dropna()
+    df1=(
+        pd.read_csv(url_or_path_to_csv_file)
+        .rename(columns={'age':'Age','sex':'Sex','bmi':'BMI','children':'Children','smoker':'Smoker','region':'Region','charges':'Insurance Premium'})
+        .dropna())
     return df1
