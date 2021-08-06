f5f4aee4755580d5f810da755d6a30a644f43f5e
Fei Song <feifeisophie_song@hotmail.com>
2021-08-06 13:35:06 -0700

    Feedback Task 2 done

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -6,3 +6,7 @@ def load_and_process(url_or_path_to_csv_file):
         .rename(columns={'age':'Age','sex':'Sex','bmi':'BMI','children':'Children','smoker':'Smoker','region':'Region','charges':'Insurance Premium'})
         .dropna())
     return df1
+
+# this is to import the data
+# then to rename the columns with the first letter capitalized
+# and to drop the Nan values
